cf0d7d5b2ac013f1df5f7d0db1a047250478d45d
Not-Secret-Dev <ayaanmumtaz8@gmail.com>
2026-02-16 02:20:39 +0500

    Landing page completed & responsiveness fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./compnents/global/navbar/Navbar.jsx";
 import Landing from "./compnents/pages/landing/Landing.jsx";
+import Footer from "./compnents/global/footer/Footer.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <Landing />
+      <Footer />
     </>
   );
 }
--- src/compnents/pages/landing/Landing.jsx
+++ src/compnents/pages/landing/Landing.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Hero from "./Hero/Hero.jsx";
 import Offers from "./Offers/Offers.jsx";
+import HomeAndOutdoor from "./Home&Outdoor/Home&Outdoor.jsx";
+import Gadgets from "./Gadgets/Gadgets.jsx";
+import Inquiry from "./Inquiry/Inquiry.jsx";
+import Recommended from "./Recommended/Recommended.jsx";
+import ExtraServices from "./ExtraServices/ExtraServices.jsx";
+import SuppliersByRegion from "./SuppliersByRegion/SuppliersByRegion.jsx";
+import Newsletter from "./Newsletter/Newsletter.jsx";
 
 const Landing = () => {
   return (
     <div>
       <Hero />
       <Offers />
+      <HomeAndOutdoor />
+      <Gadgets />
+      <Inquiry />
+      <Recommended />
+      <ExtraServices />
+      <SuppliersByRegion />
+      <Newsletter />
     </div>
   );
 };
